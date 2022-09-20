8d55a2178b5597da0dcd288b3e2061c7da03572c
mont06 <montmax06@gmail.com>
2022-09-20 22:37:25 +0300

    Доробив сайдбар без посилань

---
+++ main.js
@@ -0,0 +1,24 @@
+var dialog = document.querySelector('#dialog')
+document.querySelector('#figma').onclick = function () {
+    dialog.show()
+}
+
+document.querySelector('#figmaButton').onclick = function () {
+    dialog.show()
+}
+
+document.querySelector('#exitTop').onclick = function () {
+    dialog.close()
+}
+
+document.querySelector('#exitLeft').onclick = function () {
+    dialog.close()
+}
+
+document.querySelector('#exitRight').onclick = function () {
+    dialog.close()
+}
+
+document.querySelector('#exitBottom').onclick = function () {
+    dialog.close()
+}
